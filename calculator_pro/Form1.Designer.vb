01f8e8d062d94a715f825e44b4170b1d71be0f5b
someone <someone@someplace.com>
2021-07-30 21:48:11 -0500

    first commit

---
+++ calculator_pro/Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.display_Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.operator_Label1 = New System.Windows.Forms.Label()
+        Me.first_num_Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'display_Label1
+        '
+        Me.display_Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.display_Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.display_Label1.Location = New System.Drawing.Point(12, 42)
+        Me.display_Label1.Name = "display_Label1"
+        Me.display_Label1.Size = New System.Drawing.Size(250, 55)
+        Me.display_Label1.TabIndex = 0
+        Me.display_Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 314)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 45)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "0"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(148, 314)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 45)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(12, 263)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(50, 45)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "1"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(81, 263)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(50, 45)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "2"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(148, 263)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(50, 45)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "3"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(12, 212)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(50, 45)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "4"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(148, 110)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(50, 45)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "%"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(212, 110)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(50, 45)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "X"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button10.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button10.Location = New System.Drawing.Point(12, 110)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(119, 45)
+        Me.Button10.TabIndex = 10
+        Me.Button10.Text = "C"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button12.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button12.Location = New System.Drawing.Point(212, 161)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(50, 45)
+        Me.Button12.TabIndex = 12
+        Me.Button12.Text = "-"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button13.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button13.Location = New System.Drawing.Point(148, 161)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(50, 45)
+        Me.Button13.TabIndex = 13
+        Me.Button13.Text = "9"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button14.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button14.Location = New System.Drawing.Point(81, 161)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(50, 45)
+        Me.Button14.TabIndex = 14
+        Me.Button14.Text = "8"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button15.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button15.Location = New System.Drawing.Point(12, 161)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(50, 45)
+        Me.Button15.TabIndex = 15
+        Me.Button15.Text = "7"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button16.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button16.Location = New System.Drawing.Point(212, 212)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(50, 45)
+        Me.Button16.TabIndex = 16
+        Me.Button16.Text = "+"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button17.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button17.Location = New System.Drawing.Point(148, 212)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(50, 45)
+        Me.Button17.TabIndex = 17
+        Me.Button17.Text = "6"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button18.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button18.Location = New System.Drawing.Point(81, 212)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(50, 45)
+        Me.Button18.TabIndex = 18
+        Me.Button18.Text = "5"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button19.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button19.Location = New System.Drawing.Point(212, 263)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(50, 96)
+        Me.Button19.TabIndex = 19
+        Me.Button19.Text = "="
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'operator_Label1
+        '
+        Me.operator_Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.operator_Label1.ForeColor = System.Drawing.Color.DimGray
+        Me.operator_Label1.Location = New System.Drawing.Point(237, 9)
+        Me.operator_Label1.Name = "operator_Label1"
+        Me.operator_Label1.Size = New System.Drawing.Size(25, 23)
+        Me.operator_Label1.TabIndex = 20
+        Me.operator_Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'first_num_Label2
+        '
+        Me.first_num_Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.first_num_Label2.ForeColor = System.Drawing.Color.DimGray
+        Me.first_num_Label2.Location = New System.Drawing.Point(26, 11)
+        Me.first_num_Label2.Name = "first_num_Label2"
+        Me.first_num_Label2.Size = New System.Drawing.Size(205, 21)
+        Me.first_num_Label2.TabIndex = 21
+        Me.first_num_Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(275, 374)
+        Me.Controls.Add(Me.first_num_Label2)
+        Me.Controls.Add(Me.operator_Label1)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.display_Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents display_Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents operator_Label1 As Label
+    Friend WithEvents first_num_Label2 As Label
+End Class
---
+++ calculator_pro/Form1.vb
@@ -0,0 +1,165 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "0"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "0"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "1"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "1"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "2"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "2"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "3"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "3"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "4"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "4"
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        'Me.display_Label1.Text = Me.display_Label1.Text + "5"
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "5"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "5"
+        End If
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        'Me.display_Label1.Text = Me.display_Label1.Text + "6"
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "6"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "6"
+        End If
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        'Me.display_Label1.Text = Me.display_Label1.Text + "7"
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "7"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "7"
+        End If
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        'Me.display_Label1.Text = Me.display_Label1.Text + "8"
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "8"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "8"
+        End If
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        'Me.display_Label1.Text = Me.display_Label1.Text + "9"
+        If (Me.display_Label1.Text = "0") Then
+            Me.display_Label1.Text = "9"
+        Else
+            Me.display_Label1.Text = Me.display_Label1.Text + "9"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        'Me.display_Label1.Text = ""
+        Me.display_Label1.ResetText()
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Me.operator_Label1.Text = "+"
+
+        If (Me.display_Label1.Text = "") Then
+        Else
+            Me.first_num_Label2.Text = Me.display_Label1.Text
+        End If
+        Me.display_Label1.Text = ""
+
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Me.operator_Label1.Text = "-"
+
+        If (Me.display_Label1.Text = "") Then
+        Else
+            Me.first_num_Label2.Text = Me.display_Label1.Text
+        End If
+        Me.display_Label1.Text = ""
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Me.operator_Label1.Text = "x"
+
+        If (Me.display_Label1.Text = "") Then
+        Else
+            Me.first_num_Label2.Text = Me.display_Label1.Text
+        End If
+        Me.display_Label1.Text = ""
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Me.operator_Label1.Text = "%"
+
+        If (Me.display_Label1.Text = "") Then
+        Else
+            Me.first_num_Label2.Text = Me.display_Label1.Text
+        End If
+        Me.display_Label1.Text = ""
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        Dim fn As Double
+        Dim sn As Double
+        Dim rn As Double
+        Dim op As String
+
+        Double.TryParse(Me.first_num_Label2.Text, fn)
+        Double.TryParse(Me.display_Label1.Text, sn)
+
+        op = Me.operator_Label1.Text
+
+        Select Case op
+            Case "+"
+                rn = fn + sn
+            Case "-"
+                rn = fn - sn
+            Case "x"
+                rn = fn * sn
+            Case "%"
+                rn = fn / sn
+            Case Else
+        End Select
+
+        Me.display_Label1.Text = rn.ToString()
+        Me.first_num_Label2.ResetText()
+        Me.operator_Label1.ResetText()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.display_Label1.Text += "."
+    End Sub
+End Class
